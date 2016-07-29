44d237ec6237335eeed8f2c633bf64484b187250
nickgallimore <nfgallimore@gmail.com>
2016-07-29 14:13:04 -0400

    Renamed main file to JavaOS.java

--- JavaOS.java
+++ JavaOS.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Lp {
+public class JavaOS {
 	private static boolean stop = false;
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
